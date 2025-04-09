7aa5745eda2a098c44178b565659c146d1bb1b57
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-04-09 09:18:49 -0500

    improved asset card

--- frontend/src/components/asset-card/AssetCard.tsx
+++ frontend/src/components/asset-card/AssetCard.tsx
@@ -10,24 +10,41 @@ interface Props {
 export default function AssetCard(props: Props) {
     const asset = props.asset
 
-    return <div className='asset-card'>
-        <div className="asset-card--main">
-            <img alt={asset.name.String} src={imageNotAvailable} height={100} />
-            <div className="asset-card--details">
-                <p className="asset-card--title">{asset.name.String}</p>
-                {asset.brand.Valid && Boolean(asset.brand.String) && <p className="asset-card--sub">
-                    <strong>Brand: </strong> {asset.brand.String}
-                </p>}
-                {asset.model.Valid && Boolean(asset.model.String) && <p className="asset-card--sub">
-                    <strong>Model: </strong> {asset.model.String}
-                </p>}
-                {asset.serial.Valid && Boolean(asset.serial.String) && <p className="asset-card--sub">
-                    <strong>S/N: </strong> {asset.serial.String}
-                </p>}
-                <p className="asset-card--sub">
-                    <strong>Location: </strong> {asset.location.String}
-                </p>
+    return (
+        <div className='asset-card'>
+            <div className="asset-card--main">
+                <img
+                    className="asset-card--image"
+                    alt={asset.name.String}
+                    src={imageNotAvailable}
+                    height={100}
+                />
+                <div className="asset-card--details">
+                    <p className="asset-card--title">{asset.name.String}</p>
+
+                    {asset.brand.Valid && Boolean(asset.brand.String) && (
+                        <p className="asset-card--sub">
+                            <strong>Brand:</strong> {asset.brand.String}
+                        </p>
+                    )}
+
+                    {asset.model.Valid && Boolean(asset.model.String) && (
+                        <p className="asset-card--sub">
+                            <strong>Model:</strong> {asset.model.String}
+                        </p>
+                    )}
+
+                    {asset.serial.Valid && Boolean(asset.serial.String) && (
+                        <p className="asset-card--sub">
+                            <strong>S/N:</strong> {asset.serial.String}
+                        </p>
+                    )}
+
+                    <div className="asset-card--location-badge">
+                        {asset.location.String}
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
