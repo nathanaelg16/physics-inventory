8e756601254e1c730ba15335ee08136e7b7ce194
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-04-09 21:22:06 -0500

    added label with assets found

--- frontend/src/views/search/SearchView.tsx
+++ frontend/src/views/search/SearchView.tsx
@@ -2,7 +2,7 @@ import SearchBox from "../../components/search-box/SearchBox";
 import {useState} from "react";
 import {main} from "../../../wailsjs/go/models";
 import Asset = main.Asset;
-import {Alert, Snackbar} from "@mui/material";
+import {Alert, Snackbar, Typography} from "@mui/material";
 import "./searchView.css"
 import AssetCard from "../../components/asset-card/AssetCard";
 import {SnackbarAlert} from "../../utils/snackbar-alert";
@@ -16,8 +16,15 @@ export default function SearchView() {
             <SearchBox onResult={(results) => setResults(results)} onAlert={(a: SnackbarAlert)=> setAlert(a)} />
         </div>
         <div className="horizontal-line"></div>
+        <div className="results-count">
+            <Typography variant="body2" color="textSecondary">
+                {results.length > 0
+                    ? `Found ${results.length} asset${results.length !== 1 ? 's' : ''}`
+                    : 'No assets found'}
+            </Typography>
+        </div>
         <div id='results'>
-            {results !== null && results.map((r) => <AssetCard key={r.id} asset={r} />)}
+            {results.map((r) => <AssetCard key={r.id} asset={r}/>)}
         </div>
         <Snackbar autoHideDuration={3000} anchorOrigin={{vertical: 'bottom', horizontal: 'right'}} open={Boolean(alert)} onClose={() => setAlert(null)}>
             <Alert severity={alert?.severity}>{alert?.msg}</Alert>
