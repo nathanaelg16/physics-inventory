c92bcfcd1cfa50b6737dd961d9dc7cc43c949ada
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-05-29 17:56:12 -0500

    configuring css modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    modules: {
+      // Enable CSS modules for .module.css files
+      localsConvention: 'camelCase',
+      scopeBehaviour: 'local',
+      generateScopedName: '[name]_[local]_[hash:base64:5]'
+    }
+  }
 })
