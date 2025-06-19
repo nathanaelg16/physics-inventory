32a211c6ada3e476cc8f97fea18d6705a61c7f7f
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-06-19 16:20:58 -0500

    added log statement upon login

--- app.go
+++ app.go
@@ -51,6 +51,7 @@ func (a *App) GetProductVersion() (string, error) {
 }
 
 func (a *App) Login(username string, password string) (uint8, error) {
+	runtime.LogInfof(a.ctx, "Logging in user: %s", username)
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true", username, password, DBHost))
 	if err != nil {
 		runtime.LogError(a.ctx, err.Error())
