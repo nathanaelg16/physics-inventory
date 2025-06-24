e0a9874fc893bd531cef7d6bbef5aa1fc85beac9
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-06-23 21:02:37 -0500

    adding tls options to login view

--- app.go
+++ app.go
@@ -58,9 +58,10 @@ func (a *App) GetProductVersion() (string, error) {
 	}
 }
 
-func (a *App) Login(username string, password string) (uint8, error) {
+func (a *App) Login(username string, password string, tlsOption string) (uint8, error) {
 	runtime.LogInfof(a.ctx, "Logging in user: %s", username)
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true&tls=skip-verify", username, password, DBHost))
+	runtime.LogInfof(a.ctx, "TLS Option: %s", tlsOption)
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true&tls=%s", username, password, DBHost, tlsOption))
 	if err != nil {
 		runtime.LogError(a.ctx, err.Error())
 		return uint8(Viewer), err
