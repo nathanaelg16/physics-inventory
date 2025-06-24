476bcf7cf322b72d6254331368487882b3048a7c
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-06-23 21:02:37 -0500

    adding tls options to login view

--- app.go
+++ app.go
@@ -11,9 +11,9 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
-//const DBHost = "localhost:3306"
+const DBHost = "localhost:3306"
 
-const DBHost = "redwooddb0.cc.andrews.edu"
+//const DBHost = "redwooddb0.cc.andrews.edu"
 
 //go:embed wails.json
 var wailsJSON string
@@ -58,9 +58,10 @@ func (a *App) GetProductVersion() (string, error) {
 	}
 }
 
-func (a *App) Login(username string, password string) (uint8, error) {
+func (a *App) Login(username string, password string, tlsOption string) (uint8, error) {
 	runtime.LogInfof(a.ctx, "Logging in user: %s", username)
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true&tls=skip-verify", username, password, DBHost))
+	runtime.LogInfof(a.ctx, "TLS Option: %s", tlsOption)
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true&tls=%s", username, password, DBHost, tlsOption))
 	if err != nil {
 		runtime.LogError(a.ctx, err.Error())
 		return uint8(Viewer), err
--- frontend/wailsjs/go/main/App.js
+++ frontend/wailsjs/go/main/App.js
@@ -206,8 +206,8 @@ export function GetUsers() {
   return window['go']['main']['App']['GetUsers']();
 }
 
-export function Login(arg1, arg2) {
-  return window['go']['main']['App']['Login'](arg1, arg2);
+export function Login(arg1, arg2, arg3) {
+  return window['go']['main']['App']['Login'](arg1, arg2, arg3);
 }
 
 export function RemoveImage(arg1) {
