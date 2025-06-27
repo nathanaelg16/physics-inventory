c174e87fa16ecfe283713ec0473471b30063ef46
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-06-27 11:27:49 -0500

    renaming variable so that it is not exported

--- app.go
+++ app.go
@@ -11,9 +11,9 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
-//const DBHost = "localhost:3306"
+//const dbHost = "localhost:3306"
 
-const DBHost = "redwooddb0.cc.andrews.edu"
+const dbHost = "redwooddb0.cc.andrews.edu"
 
 //go:embed wails.json
 var wailsJSON string
@@ -60,7 +60,7 @@ func (a *App) GetProductVersion() (string, error) {
 
 func (a *App) Login(username string, password string) (uint8, error) {
 	runtime.LogInfof(a.ctx, "Logging in user: %s", username)
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true", username, password, DBHost))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/physics_inventory?parseTime=true", username, password, dbHost))
 	if err != nil {
 		runtime.LogError(a.ctx, err.Error())
 		return uint8(Viewer), err
